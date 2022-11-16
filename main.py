6ed8567c1cb4cc95943a644560a04befedfad64e
Dreammary0 <sazontova.md@students.dvfu.ru>
2022-11-16 14:50:29 +1000

    Оформила функции

--- main.py
+++ main.py
@@ -1,92 +1,53 @@
+from jinja2 import Template, Environment, FileSystemLoader
 import sqlite3
-con = sqlite3.connect("beautyfull.sqlite")
-f_damp = open('Beauty.sql','r', encoding ='utf-8-sig')
-damp = f_damp.read()
-f_damp.close()
-# запускаем запросы
-#con.executescript(damp)
-# сохраняем информацию в базе данных
-#con.commit()
-# создаем курсор
-cursor = con.cursor()
-# выбираем и выводим записи из таблиц author, reader
+from sqlite3 import Error
+from Beauty_Model import *
+
+#создание соединения
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+con = create_connection("beautyfull.sqlite")
 
 
-print("1.1 Вывести услуги для процедуры Парикмахер")
-cursor.execute("""SELECT ProcedureListName , ProcedurePrice, P.ProcedureName  
-FROM ProcedureList
-JOIN "Procedure" P on P.IDProcedure = ProcedureList.Procedure_IDProcedure 
-WHERE Procedure_IDProcedure=1 
-order by ProcedurePrice ASC 
-""")
-print(cursor.fetchall())
 
-print("1.2 Вывести мастеров процедуры")
-cursor.execute("""
-SELECT MasterName , P.ProcedureName
-FROM Master
-JOIN "Procedure" P on P.IDProcedure = Master.Procedure_IDProcedure
-Where Procedure_IDProcedure=2
-ORDER BY MasterName ASC 
-""")
-print(cursor.fetchall())
+#print(Task1(con))
+#print(Task2(con))
+#print(Task3(con))
+#print(Task4(con))
+#print(Task5(con))
+#print(Task6(con))
+Task7(con)
+#Task8(con)
 
 
-print("2.1 Сколько процедур у каждой услуги")
-cursor.execute("""
-Select P.ProcedureName, count(IDProcedureList) 
-from ProcedureList 
-JOIN "Procedure" P on P.IDProcedure = ProcedureList.Procedure_IDProcedure 
-GROUP BY P.ProcedureName
-""")
-print(cursor.fetchall())
 
-print("2.1 Какие даты есть у мастера")
-cursor.execute('''
-select Date, StartTime, EndTime, M.MasterName, P.ProcedureName
-from Schedule
-JOIN Procedure P on P.IDProcedure = M.Procedure_IDProcedure
-JOIN Master M on M.IDMaster = Schedule.Master_IDMaster
-Where Master_IDMaster=1
-GROUP BY Date
-''')
-print(cursor.fetchall())
+def Create_Home():
+    # Шаблон для главной страницы
+    f_template = open('templates/Home_templ.html', 'r', encoding='utf-8-sig')
+    html = f_template.read()
+    f_template.close()
+    # создаем объект-шаблон
+    template = Template(html)
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template('templates/Home_templ.html')
+    template.globals['len'] = len
+    template.globals['print'] = print
 
+    # генерируем результат на основе шаблона
+    result_html = template.render(
+        len=len
+    )
 
-print("3.1 Самая дорогая процедура")
-cursor.execute('''
-with test as (select IDProcedureList, max(ProcedurePrice) as price from ProcedureList)
-select P2.ProcedureName, P.ProcedureListName, t.price
-from ProcedureList P, test t 
-join "Procedure" P2 on P2.IDProcedure = P.Procedure_IDProcedure
-where P.IDProcedureList = t.IDProcedureList
-
-''')
-print(cursor.fetchall())
-
-
-print("3.2 Вывести доступные варианты записи на процедуру 1 после 18:00 и мастеров")
-cursor.execute('''
-with night as (select * from OrderList where Procedure_IDProcedure=1 and OrderTime>=18 and Client_IDClient is NULL)
-select IDOrder, P.ProcedureName, OrderData, OrderTime,M.MasterName, M.MasterPhone 
-from night
-join Schedule S on night.Schedule_IDSchedule=S.IDSchedule
-join Master M on S.Master_IDMaster=M.IDMaster
-join Procedure P on M.Procedure_IDProcedure=P.IDProcedure
-''')
-print(cursor.fetchall())
-
-
-print("4.1 Добавить клиента Андрей")
-cursor.executescript('''
-INSERT INTO Client ("IDClient","ClientName","ClientPhone") 
-VALUES (null, 'Андрей','556374')
-''')
-
-print("4.2 Записать клиента на процедуру")
-cursor.executescript('''
-UPDATE OrderList SET Client_IDClient = 1 WHERE IDOrder=1
-''')
-
-# закрываем соединение с базой
-con.close()
+    # создаем файл для HTML-страницы
+    f = open('pages/Home.html', 'w', encoding='utf-8-sig')
+    # выводим сгенерированную страницу в файл
+    f.write(result_html)
+    f.close()
